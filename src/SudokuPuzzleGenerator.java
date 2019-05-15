679bdf6a261bdf82f84a60c2cae8117d5e2c1620
Shobhit <smundra42@gmail.com>
2019-05-14 23:18:36 -0500

    Code is working more faster. A bug needs to be fixed

--- src/SudokuPuzzleGenerator.java
+++ src/SudokuPuzzleGenerator.java
@@ -7,12 +7,12 @@ import java.util.Set;
 
 public class SudokuPuzzleGenerator {
 
-    public Cell[][] generatePuzzle(String filePath)
+    public SudokuPuzzle generatePuzzle(String filePath)
             throws IllegalCharacterException, IOException, InvalidPuzzleException {
         //Puzzle-9x9-0401.txt
         //Puzzle-9x9-0901
         //Puzzle-9x9-0101.txt
-        File file = new File("C:\\Users\\Shobhit\\Desktop\\repos\\Sudoku-Solver\\src\\puzzles\\Puzzle-9x9-0001.txt");
+        File file = new File("C:\\Users\\Shobhit\\Desktop\\repos\\Sudoku-Solver\\src\\puzzles\\Puzzle-16x16-0001.txt");
         BufferedReader reader = new BufferedReader(new FileReader(file));
         final int PUZZLE_SIZE = Integer.valueOf(reader.readLine());
         if(!new PuzzleSizeValidator().isSizeIsValid(PUZZLE_SIZE)) {
@@ -20,8 +20,8 @@ public class SudokuPuzzleGenerator {
         }
 
         Set<Character> validCharacters = generateValidCandidates(PUZZLE_SIZE, reader);
-        Cell[][] puzzle = generatePuzzleUtil(PUZZLE_SIZE, reader, validCharacters);
-        boolean isPuzzleValid = new SudokuPuzzleValidator(validCharacters, puzzle, PUZZLE_SIZE).isValid();
+        SudokuPuzzle puzzle = generatePuzzleUtil(PUZZLE_SIZE, reader, validCharacters);
+        boolean isPuzzleValid = new SudokuPuzzleValidator(validCharacters, puzzle.getSudokuPuzzle(), PUZZLE_SIZE).isValid();
         if(!isPuzzleValid){
             throw new InvalidPuzzleException("Sudoku Puzzle is not valid");
         }
@@ -40,10 +40,11 @@ public class SudokuPuzzleGenerator {
         return validCharSet;
     }
 
-    private Cell[][] generatePuzzleUtil(int size, BufferedReader reader, Set<Character> validCharacters) throws IOException, IllegalCharacterException {
+    private SudokuPuzzle generatePuzzleUtil(int size, BufferedReader reader, Set<Character> validCharacters) throws IOException, IllegalCharacterException {
         Cell[][] puzzle = new Cell[size][size];
         String line;
         int row = 0;
+        int cellToBeFilled = 0;
         while ((line = reader.readLine()) != null){
             String []values = line.split(" ");
             for(int col = 0; col < size; col++){
@@ -53,6 +54,8 @@ public class SudokuPuzzleGenerator {
                     for(Character value : validCharacters){
                         possibleValues.add(value);
                     }
+
+                    cellToBeFilled++;
                 }else{
                     if(!validCharacters.contains(values[col].charAt(0))){
                         throw new IllegalCharacterException("Valid character list does not have "+ values[col]);
@@ -68,6 +71,6 @@ public class SudokuPuzzleGenerator {
             row++;
         }
 
-        return puzzle;
+        return new SudokuPuzzle(puzzle, cellToBeFilled);
     }
 }
--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -5,93 +5,71 @@ import java.util.List;
 
 public class SudokuSolver {
 
-    private Cell[][] sudokuPuzzle;
+    private SudokuPuzzle sudokuPuzzle;
     private List<PuzzleSolvingStrategy> solvingStrategies;
     private SudokuPuzzleGenerator puzzleGenerator;
     private List<SolvedPuzzle> solvedPuzzle;
-    private Enum state;
 
     public SudokuSolver(){
         this.puzzleGenerator = new SudokuPuzzleGenerator();
         solvedPuzzle = new ArrayList<>();
-        state = SudokuState.UNSOLVED;
         initializeStrategy();
     }
 
     private void initializeStrategy() {
         solvingStrategies = Arrays.asList(new RowEliminationStrategy(), new ColumnEliminationStrategy(),
-                new BlockEliminationStrategy());
-                //new HiddenSingleStrategy(), new BoxReductionStrategy(), new NakedPairStrategy());
-
+                new BlockEliminationStrategy(), new HiddenSingleStrategy(), new BoxReductionStrategy(),
+                new NakedPairStrategy());
     }
 
 
     public List<SolvedPuzzle> solve(String path) throws InvalidPuzzleException, IOException, IllegalCharacterException {
         sudokuPuzzle = puzzleGenerator.generatePuzzle(path);
-        while(state == SudokuState.UNSOLVED){
+        while(sudokuPuzzle.getState() == SudokuState.UNSOLVED){
             if(!solvePuzzleUsingStrategy()){
-                solvePuzzleUsingBruteForce();
+                solvePuzzleUsingGuessStrategy();
             }
         }
 
         return solvedPuzzle;
     }
 
-    private void solvePuzzleUsingBruteForce() {
-        BruteForceSolver solver = new BruteForceSolver(sudokuPuzzle);
+    private void solvePuzzleUsingGuessStrategy() {
+        GuessStrategy solver = new GuessStrategy(sudokuPuzzle);
         solver.solve();
         printPuzzle();
     }
 
     private boolean solvePuzzleUsingStrategy(){
-        int size = sudokuPuzzle.length;
+        Cell[][] puzzle = sudokuPuzzle.getSudokuPuzzle();
         int strategyNumber = 0;
-        boolean puzzleIsSolved = false;
-        while(!puzzleIsSolved && strategyNumber < solvingStrategies.size()){
+        while(sudokuPuzzle.getState() == SudokuState.UNSOLVED && strategyNumber < solvingStrategies.size()){
             if(solvingStrategies.get(strategyNumber).solve(sudokuPuzzle)){
-                strategyNumber = 0;
+                if(strategyNumber != 0){
+                    strategyNumber = 0;
+                }else{
+                    strategyNumber++;
+                }
+
             }else{
                 strategyNumber++;
             }
 
             printPuzzle();
-            puzzleIsSolved = isPuzzleSolved();
-            if(puzzleIsSolved){
-                state = SudokuState.SOLVED;
-                puzzleIsSolved = true;
-                solvedPuzzle.add(new SolvedPuzzle(sudokuPuzzle, solvingStrategies));
+            if(sudokuPuzzle.getState() == SudokuState.SOLVED){
+                solvedPuzzle.add(new SolvedPuzzle(puzzle, solvingStrategies));
                 break;
             }
         }
 
-        return puzzleIsSolved;
-    }
-
-    private boolean isPuzzleSolved() {
-
-        for(int i = 0; i < sudokuPuzzle.length; i++){
-
-            for(int j = 0; j < sudokuPuzzle.length; j++){
-
-                Cell currentCell =sudokuPuzzle[i][j];
-                //System.out.print(currentCell.getCandidates() + " ");
-                if(currentCell.getSize() != 1){
-                    return false;
-                }
-
-            }
-
-            //System.out.println();
-        }
-
-        return true;
+        return sudokuPuzzle.getState() == SudokuState.SOLVED;
     }
 
     private void printPuzzle(){
-        for (int i = 0; i < sudokuPuzzle.length; i++){
+        for (int i = 0; i < sudokuPuzzle.getSudokuPuzzle().length; i++){
 
-            for(int j = 0; j < sudokuPuzzle.length; j++){
-                System.out.print(sudokuPuzzle[i][j].getCandidates()+ " ");
+            for(int j = 0; j < sudokuPuzzle.getSudokuPuzzle().length; j++){
+                System.out.print(sudokuPuzzle.getSudokuPuzzle()[i][j].getCandidates()+ " ");
             }
 
             System.out.println();
